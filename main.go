11f124e37a8053be2595acd8e240613fa6761443
charlie cochrane <cochrane26@gmail.com>
2017-06-05 22:46:15 +0100

    added level logger

--- main.go
+++ main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-	"log"
 	"net"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 )
 
-var MTU = 1490
+var bufferSize = 1490
 
 func main() {
+	log.Infoln("UDP Packet Duplicator")
+
 	host, remotes, err := getPorts(os.Args[1:])
 	checkErr(err)
+
+	log.Infoln("Host Port:", host)
+	log.Infoln("Remote Ports:", remotes)
 
 	hostConn, err := net.ListenUDP("udp", host)
 	checkErr(err)
 	defer hostConn.Close()
 
-	log.Println("UDP Packet Duplicator")
-	log.Println("Host Port:", host)
-	log.Println("Remote Ports:", remotes)
 	proxy(hostConn, remotes)
 }
 
 func checkErr(err error) {
 	if err != nil {
-		log.Fatalln("Fatal Error:", err)
+		log.Fatalln(err)
 	}
 }
 
 func proxy(sock *net.UDPConn, forwards []*net.UDPAddr) {
-	buffer := make([]byte, MTU)
+	buffer := make([]byte, bufferSize)
 
 	for {
 		bytes, _, err := sock.ReadFromUDP(buffer)
 		if err != nil {
-			log.Println("Error: unable to read UDP packet payload:", err)
+			log.Errorln(err)
 			continue
 		}
 		if bytes == 0 {
-			log.Println("Error: unable to read UDP packet, zero bytes in size")
+			log.Errorln("zero byte packet")
 			continue
 		}
 
-		log.Println("Forwarding", bytes, " byte packet")
+		log.Infoln(bytes, "byte packet recieved, forwarding")
 
 		for _, addr := range forwards {
-			sock.WriteToUDP(buffer[0:bytes], addr)
+			_, err := sock.WriteToUDP(buffer[0:bytes], addr)
+			if err != nil {
+				log.Errorln(err)
+				continue
+			}
 		}
 	}
 }
